d88f770bf97d66b5b0004f44d503cfb081ed298c
Stephanie <stephaniebarreyro@gmail.com>
2015-05-05 15:56:50 -0700

    Add Reviews Migration.

---
+++ db/migrate/20150505224941_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :email, null: false
+      t.integer :rating
+      t.references :movie
+      t.timestamps
+    end
+  end
+end
