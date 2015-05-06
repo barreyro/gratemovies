77717ebf4200723c043ee009d0bb05323f95e817
Stephanie <stephaniebarreyro@gmail.com>
2015-05-05 20:40:44 -0700

    Add comment as an attribute to reviews migration.

--- db/migrate/20150505224941_create_reviews.rb
+++ db/migrate/20150505224941_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.string :email, null: false
       t.integer :rating
+      t.text :comment
       t.references :movie
       t.timestamps
     end
