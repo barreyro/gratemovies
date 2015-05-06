17c19ef99654efc2de24f4ba2e8bf45102a173c4
Stephanie <stephaniebarreyro@gmail.com>
2015-05-05 20:41:25 -0700

    Create review model with working validations.

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ActiveRecord::Base
+  validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :rating, presence: true
+  belongs_to :movie
+end
