a946a0c9c4545e2fe04ae43de59ac1498acb3852
Stephanie <stephaniebarreyro@gmail.com>
2015-05-06 05:46:42 -0700

    Add review controller and specs pass.

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -18,24 +18,61 @@ describe ReviewsController do
  end
 
  describe 'GET #show' do
-    it "assigns the requested review to @review"
-    it "renders the :show template"
+    it "assigns the requested review to @review" do
+       review = FactoryGirl.create(:review)
+       get :show, id: review
+       expect(assigns(:review)).to eq review
+    end
+
+    it "renders the :show template" do
+       review = FactoryGirl.create(:review)
+       get :show, id: review
+       expect(response).to render_template :show
+    end
  end
 
  describe 'GET #new' do
-    it "assigns a new Review to @review"
-    it "renders the :new template"
+    it "assigns a new Review to @review" do
+      get :new
+      expect(assigns(:review)).to be_a_new(Review)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
  end
 
  describe "POST #create" do
+
    context "with valid attributes" do
-      it "saves the new review in the database"
-      it "redirects to reviews#show"
+      it "saves the new review in the database" do
+        expect{
+          post :create,
+           review: FactoryGirl.attributes_for(:review)
+        }.to change(Review, :count).by(1)
+      end
+
+      it "redirects to reviews#show" do
+         post :create,
+          review: FactoryGirl.attributes_for(:review)
+         expect(response).to redirect_to review_path(assigns[:review])
+      end
    end
 
    context "with invalid attributes" do
-      it "does not save the new review in the database"
-      it "re-renders the :new template"
+      it "does not save the new review in the database" do
+          expect{
+            post :create,
+            review: FactoryGirl.attributes_for(:invalid_review)
+          }.to_not change(Review, :count)
+      end
+
+      it "re-renders the :new template" do
+          post :create,
+            review: FactoryGirl.attributes_for(:invalid_review)
+          expect(response).to render_template :new
+      end
    end
  end
 end
